f36f19915b0137de1b1b4faa66e377fdfaf85989
mkginfo <mohit0313@gmail.com>
2022-08-08 22:28:57 +0200

    Added episodes name with season in the  header text

--- src/components/ui/Episodes.vue
+++ src/components/ui/Episodes.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="pl-4 pr-4 pt-2 mt-5">
-    <h3 class="text-secondary">{{ episodesList[0].name}} - Season {{ seasonNumber }} episodes</h3>
+    <h3 class="text-secondary">{{ getEpisodesName()}} - Season {{ seasonNumber }} episodes</h3>
     <div class="row">
       <div
         class="col-md-2 col-6 mt-4"
@@ -18,16 +18,24 @@ import Image from "@/components/form/Image.vue";
 import {defineComponent} from "vue";
 import type {PropType} from "vue";
 import type {Episodes} from "@/models/tvmaze.model";
+
 export default defineComponent({
   components: {
     Image
   },
   props: {
     seasonNumber: {
-      type: [Number, String]
+      type: [Number, String],
+      required: true
     },
     episodesList: {
       type: Array as PropType<Episodes[]>,
+      required: true
+    }
+  },
+  methods: {
+    getEpisodesName(): string {
+      return this.episodesList?.length > 0 ? this.episodesList[0].name : "";
     }
   }
 });
