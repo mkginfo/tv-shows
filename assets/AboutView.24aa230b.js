09211eb970e8389ce83109c09d238348358e535b
mkginfo <mohit0313@gmail.com>
2022-08-08 02:26:56 +0200

    Updates

---
+++ assets/AboutView.24aa230b.js
@@ -0,0 +1,1 @@
+import{_ as e,c as t,o,a as s}from"./index.62b1a6b9.js";const _={},c={class:"about"},a=s("h1",null,"This is an about page",-1),n=[a];function i(r,u){return o(),t("div",c,n)}const l=e(_,[["render",i]]);export{l as default};
---
+++ assets/default-image.7538c0a6.js
@@ -0,0 +1,1 @@
+const s="/tv-shows/assets/default-image.77336935.png";export{s as _};
