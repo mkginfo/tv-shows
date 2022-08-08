24cc0d258d1050f97f433310f6d71ca85dfbcf21
mkginfo <mohit0313@gmail.com>
2022-08-08 02:22:47 +0200

    fixed layout issue

--- src/components/organisms/MazePopular.vue
+++ src/components/organisms/MazePopular.vue
@@ -20,26 +20,10 @@
       </OverviewSection>
   </div>
 
-  <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
-    <div class="carousel-inner">
-      <div class="carousel-item  h-25" :class="{ active: index===0 }" v-for="(show, index) in popularShows" :key="index" :index="index">
-        <img :src="show.image.medium || show.image.original" class="d-block w-100 h-25" alt="...">
-      </div>
-    </div>
-    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Previous</span>
-    </button>
-    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Next</span>
-    </button>
-  </div>
 </template>
 <script lang="ts">
 import { RouterLink } from "vue-router";
 import OverviewSection from "@/components/OverviewSection.vue";
-import * as Bootstrap from 'bootstrap'
 import {defineComponent} from "vue";
 export default defineComponent({
   components: {OverviewSection},
@@ -47,10 +31,6 @@ export default defineComponent({
     popularShows: { type: Array, required: true }
   },
   data: () => ({ ratedBy: 10 }),
-  mounted() {
-    var myCarousel = document.querySelector('#carouselExampleCaptions')
-    new Bootstrap.Carousel(myCarousel)
-  }
 });
 </script>
 <style scoped>
