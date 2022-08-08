87a0478518f29e4140b538002aa0137d3adfdb85
mkginfo <mohit0313@gmail.com>
2022-08-08 09:43:22 +0200

    Updates

--- assets/AboutView.69ad2b77.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c as t,o,a as s}from"./index.1a8ba83c.js";const _={},c={class:"about"},a=s("h1",null,"This is an about page",-1),n=[a];function i(r,u){return o(),t("div",c,n)}const l=e(_,[["render",i]]);export{l as default};
